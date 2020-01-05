ccba5fb8fb935f7988cdc4733f30110a39b5ec4a
Ric7Arman <armando.rc_@hotmail.com>
2020-01-04 21:48:06 -0500

    commit login-app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React , { ReactDOM } from 'react';
+import './styles/styles.scss'
+
+const App = () => (
+  <>
+    <form className="ed-container">
+      <div className="ed-item">
+        <label>User</label>
+        <input type="text"></input>
+      </div>
+      <div className="ed-item">
+        <label>Password</label>
+        <input type="text"></input>
+      </div>
+
+    </form>
+  </>
+)
+
+
+
+
+export default App;
